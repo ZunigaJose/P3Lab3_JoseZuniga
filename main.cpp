c835460598f610829b4a45f623a7a6d8f7febff6
Jose Zuniga <zunigajose@localhost.localdomain>
2020-02-07 13:55:53 -0600

    ejercicio 1 ahora si!

--- main.cpp
+++ main.cpp
@@ -4,37 +4,39 @@ using namespace std;
 
 int triangu(int n) {
 	int nTriangular = 0;
-	for(i = 1; i < n; i++) {
+	for(int i = 1; i < n; i++) {
 		if(nTriangular + i > n){
 			break;
 		} else {
 			nTriangular += i;
 		}
 	}
+	return nTriangular;
 }
 
 int menu() {
 	int op;
-	int usoVario;
 	cout << "Lab 3 - Jose Zuniga -" << endl;
-	cout << "0. Salir\n1. Identificar numero triangular previo\n
-		2. Calcular area de triangulo\n3. Calcular area de circulo\n
-		4. Calcular volumen de piramide\n5. Calcular volumen de esfera: ";
+	cout << "0. Salir\n1. Identificar numero triangular previo\n";
+	cout << "2. Calcular area de triangulo\n3. Calcular area de circulo\n";
+	cout << "4. Calcular volumen de piramide\n5. Calcular volumen de esfera: ";
 	cin >> op;
 	if(op < 0 || op > 4) return menu();
 	return op;
 }
 
 int main() {
+	int usoVario;
 	int op;
 	do {
+		op = menu();
 		switch (op) {
 			case 1:
 				cout << "Ingrese el un numero m: ";
-				cin << usoVario;
-				cout << "El numero triangular previo es: " << triangu(usoVario);
+				cin >> usoVario;
+				cout << "numero triangular: " << triangu(usoVario) << endl << endl;
 			break;
-
+		}
 	} while (op);
 	return 0;
 }
